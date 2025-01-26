b8a4bd96e25df1fa4a6e0d2eabbd1a3dde327ef6
Celeste Hayes <celestehayes@Mac.attlocal.net>
2025-01-26 16:10:31 -0500

    feat: add thought model schema

---
+++ src/models/Thought.ts
@@ -0,0 +1,38 @@
+import { Schema, model } from "mongoose";
+import reactionSchema from "./Reaction";
+
+const dateFormat = (timestamp: Date) => {
+  return timestamp.toLocaleString();
+};
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      maxlength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timestamp: Date) => dateFormat(timestamp),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+  });
+const Thought = model("Thought", thoughtSchema);
+
+export default Thought;
