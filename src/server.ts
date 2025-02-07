e591139e68a71e6110ded07092db3b1508c59641
Celeste Hayes <celestehayes@Mac.attlocal.net>
2025-02-06 18:05:28 -0600

    Refactor: Adjustments to files to run server

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,17 @@
-const express = require("express");
-const db = require("./config/connection");
-const routes = require("./routes");
+import express from 'express';
+import routes from './routes/index.js';
+import db from './config/connection.js';
+
+await db();
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
+
 app.use(routes);
 
-db.once("open", () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-  });
+app.listen(PORT, () => {
+  console.log(`API Server running on port ${PORT}!`);
 });
